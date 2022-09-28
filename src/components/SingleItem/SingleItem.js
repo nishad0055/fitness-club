da5001e8e847864ce9c3bc7d7e7cea8a27ebfeae
nishadahmed5 <nishatahmed650@gmail.com>
2022-09-28 23:06:45 +0600

    items add

--- src/components/SingleItem/SingleItem.js
+++ src/components/SingleItem/SingleItem.js
@@ -11,9 +11,9 @@ const SingleItem = (props) => {
                 <div className="card-body">
                     <h2 className="card-title"> {name}</h2>
                     <p>{text}</p>
-                    <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div> 
-                    <div className="badge badge-outline">Products</div>
+                    <div className="card-actions">
+                    <div className="badge badge-outline">For age: {age}</div> 
+                    <div className="badge badge-outline">Time: {time}</div>
                     </div>
                 </div>
                 </div>
